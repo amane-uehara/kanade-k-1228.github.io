81278f4e3e8e10ae7a7218c6c806b50160b69716
kanade-k-1228 <kanade.k.1228@gmail.com>
2023-05-25 15:54:19 +0900

    add command

--- .common/command.js
+++ .common/command.js
@@ -1,10 +1,29 @@
-const history = [];
-const command = ["ArrowUp","ArrowUp","ArrowDown","ArrowDown","ArrowLeft","ArrowRight","ArrowLeft","ArrowRight","b","a"];
-const url = "https://example.com";
-document.addEventListener('keypress',(e)=>{
-    history = [...history, e.key];
+const urls = [
+    "https://www.nicovideo.jp/watch/sm41256311",
+    "https://www.nicovideo.jp/watch/sm41310470",
+]
+
+const arrow = (s) =>{
+    if(s==="ArrowUp") return "U";
+    else if(s==="ArrowDown") return "D";
+    else if(s==="ArrowLeft") return "L";
+    else if(s==="ArrowRight") return "R";
+    else return s;
+}
+
+const command = "UUDDLRLRba";
+
+let history = "";
+
+document.addEventListener('keydown',(e)=>{
+    const key = arrow(e.key);
+    history = `${history}${key}`;
+    if(history.length>10){
+        history = history.slice(-10);
+    }
     console.log(history);
     if(history === command){
-        window.open(url,'_blank').focus();
+        const rand_index = Math.floor(Math.random() * urls.length);
+        window.open(urls.at(rand_index),'_blank').focus();
     }
 })
