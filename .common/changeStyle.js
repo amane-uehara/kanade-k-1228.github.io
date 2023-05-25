4122c37aa8539015fc57d4d0e7be070326565633
kanade-k-1228 <kanade.k.1228@gmail.com>
2023-05-25 15:03:02 +0900

    save

--- .common/changeStyle.js
+++ .common/changeStyle.js
@@ -1,6 +1,12 @@
+const light = "https://kanade-k-1228.github.io/.common/light.css";
+const dark = "https://kanade-k-1228.github.io/.common/dark.css";
+
+window.addEventListener('load',()=>{
+    console.log(allCookies)
+    document.cookie
+})
+
 const changeStyle = ()=>{
     const css = document.getElementById("style");
-    const light = "https://kanade-k-1228.github.io/.common/style.css";
-    const dark = "https://kanade-k-1228.github.io/.common/dark.css";
     css.href = css.href === light ? dark : light;
 }
