5c5b0fa5130021dd5a16541636efe5d92c2d229e
kanade-k-1228 <kanade.k.1228@gmail.com>
2023-05-25 16:10:03 +0900

    add url

--- .common/command.js
+++ .common/command.js
@@ -1,8 +1,3 @@
-const urls = [
-    "https://www.nicovideo.jp/watch/sm41310470",
-    "https://www.nicovideo.jp/watch/sm41256311"
-]
-
 const keyToChar = (s) =>{
     if(s==="ArrowUp") return "U";
     else if(s==="ArrowDown") return "D";
@@ -11,16 +6,27 @@ const keyToChar = (s) =>{
     else if(s==="Control") return "C";
     else if(s==="Shift") return "S";
     else if(s==="Alt") return "A";
+    else if(s==="Backspace") return "B";
     else return s;
 }
 
 const command = "UUDDLRLRba";
+const urls = [
+    "https://www.nicovideo.jp/watch/sm41310470",
+    "https://www.nicovideo.jp/watch/sm41256311",
+    "https://www.nicovideo.jp/watch/sm3127113"
+]
 
 let history = "";
 
 document.addEventListener('keydown',(e)=>{
     const key = keyToChar(e.key);
-    history = `${history}${key}`;
+    if(key==="B"){
+        history = history.slice(0,-1);
+    }else{
+        history = `${history}${key}`;
+    }
+    console.log(history);
     if(history.slice(-command.length) === command){
         const rand_index = Math.floor(Math.random() * urls.length);
         window.open(urls.at(rand_index),'_blank').focus();
