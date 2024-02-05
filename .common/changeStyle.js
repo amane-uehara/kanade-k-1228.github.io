39571edc80d74c7771bc80c585e90f4dc809555f
kanade-k-1228 <kanade-k-1228@users.noreply.github.com>
2024-02-05 03:14:31 +0000

    deploy: b7255f208fd2e6773e805d08eeb07106d89456cb

--- .common/changeStyle.js
+++ .common/changeStyle.js
@@ -1,20 +1,30 @@
 const light = "https://kanade-k-1228.github.io/.common/light.css";
 const dark = "https://kanade-k-1228.github.io/.common/dark.css";
 
-window.addEventListener('load',()=>{
-    console.log(document.cookie)
-})
+window.addEventListener("load", () => {
+  const style = document.cookie
+    .split("; ")
+    .find((key) => key.startsWith("style"))
+    .split("=")[1];
+  console.log(style);
+  setCss(style);
+});
 
-window.addEventListener('keydown',(e) => {
-    const css = document.getElementById("style");
-    if(e.key==="l"){
-        css.href = light;
-        document.cookie = "style=light"
-        console.log(document.cookie)
-    }
-    if(e.key==="d"){
-        css.href = dark;
-        document.cookie = "style=dark"
-        console.log(document.cookie)
-    }
-})
+window.addEventListener("keydown", (e) => {
+  const css = document.getElementById("style");
+  if (e.key === "l") setCss("light");
+  if (e.key === "d") setCss("dark");
+});
+
+const setCss = (style) => {
+  const css = document.getElementById("style");
+  if (style === "light") {
+    css.href = light;
+    document.cookie = "style=light";
+  } else if (e.key === "dark") {
+    css.href = dark;
+    document.cookie = "style=dark";
+  } else {
+    setCss("light");
+  }
+};
